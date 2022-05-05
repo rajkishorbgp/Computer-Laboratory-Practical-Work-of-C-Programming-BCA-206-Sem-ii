d09286cdee90bfe2bdec45a9aceadfe7bc994db4
Raj kishor <rajkishor6091@gmail.com>
2022-05-06 00:43:47 +0530

    find out the smallest number using pointer.

---
+++ Q10.c
@@ -0,0 +1,24 @@
+/*                                     Program:- 10
+     Write a C program to accept 5 numbers, store them in array and find out the smallest number 
+                              using pointer.
+							                                                                 */
+#include<stdio.h>
+#define n 5
+void main()
+{
+	int array[n],*small,i;
+	printf("Enter %d integer number: ",n);
+	for ( i = 0; i <n; i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	small = &array[0];
+	for ( i = 0; i <n; i++)
+	{
+		if (*small>array[i])
+		{
+			*small=array[i];
+		}
+	}
+    printf("Smallert number : %d",*small);
+}
