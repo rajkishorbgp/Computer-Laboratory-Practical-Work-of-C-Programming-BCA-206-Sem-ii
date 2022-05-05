ce1160c7dc8479a75e57dfa12789c949a8a09d7f
Raj kishor <rajkishor6091@gmail.com>
2022-05-06 00:44:46 +0530

    reverse the element of the array (Dynamic array)

---
+++ Q11.c
@@ -0,0 +1,22 @@
+/*                                 Program:- 11
+      Write a C program to take the input into an array and reverse the element of the array (Dynamic array).
+	                                                                                       */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *array,n;
+    printf("Enter the size of the array you want to create : ");
+	scanf("%d",&n);
+	array=(int*)malloc(n*sizeof(int));
+	printf("Enter the element: ");
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	printf("_____Reverse the element of Array____\n");
+	for (int i = n-1; i>= 0; i--)
+	{
+		printf("%d\n",array[i]);
+	}
+}
