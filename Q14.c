dd7f3f6a89271285824f994e605da10b892d9f4f
Raj kishor <rajkishor6091@gmail.com>
2022-05-18 13:15:46 +0530

    count total number of digits

---
+++ Q14.c
@@ -0,0 +1,13 @@
+/*                         Program: 14             
+      C program to read string and count total number of digits in that string.
+                                                                                 */
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char name[100];
+	printf("Enter string: ");
+	gets(name);
+	int digits = strlen(name);
+	printf("%d",digits);
+}
