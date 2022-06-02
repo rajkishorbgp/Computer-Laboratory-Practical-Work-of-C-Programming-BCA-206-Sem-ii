ca42c01aad979515cd544397f7bb1d46187fd3ad
Raj kishor <rajkishor6091@gmail.com>
2022-06-02 21:33:49 +0530

    replace all occurrences of character 'a' with '*' symbol

---
+++ Q19.c
@@ -0,0 +1,21 @@
+/*                               Program:19
+     Write a C program to accept a string & replace all occurrences of character 'a' with '*' symbol.
+                                                                                          */
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+     char str[50],i;
+     printf("Enter String: ");
+     gets(str);
+     for ( i = 0; i <strlen(str); i++)
+     {
+          if (str[i]=='a')
+          {
+               str[i]='*';
+          }
+     }
+     printf("replace all occurrences of character 'a' with '*' symbol\n");
+     puts(str);
+     return 0;
+}
