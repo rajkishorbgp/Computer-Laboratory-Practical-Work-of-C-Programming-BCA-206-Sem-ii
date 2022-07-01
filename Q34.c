62cbb315b93909dd343380fb693ac7d128210f73
Raj kishor <rajkishor6091@gmaio.com>
2022-07-01 12:38:08 +0530

    Write a c program for finding the largest integer among three integers using command line arguments.

---
+++ Q34.c
@@ -0,0 +1,18 @@
+/*                     Program: 34
+     Write a c program for finding the largest integer among three integers using command line arguments.                
+                                                                                                                  */
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+     int i,max=0;
+     for ( i =1; i <argc; i++)
+     {
+          if (atoi(argv[i])>max)
+          {
+               max=atoi(argv[i]);
+          }
+     }
+     printf("Largest number: %d",max);
+    return 0;
+}
