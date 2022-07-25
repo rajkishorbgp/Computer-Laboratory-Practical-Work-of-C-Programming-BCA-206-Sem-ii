180e1b925eb37400fdb9f74d383be28a2eef4a7d
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:43:19 +0530

    C program to find sum of two matrices.

--- Q6.c
+++ Q6.c
@@ -1,32 +1,35 @@
 /*                                  Program:- 6
-                         C program to find sum of two matrices.
-                                                                                        */
-#include<stdio.h>
+						 C program to find sum of two matrices.
+																						*/
+#include <stdio.h>
 void main()
 {
-    int matrix_1[3][3],matrix_2[3][3];
-	printf("Enter the first matrix:- ");
-	for (int i = 0; i < 3; i++)
+	int matrix_1[3][3], matrix_2[3][3], i, j;
+
+	printf("Enter the first matrix: ");
+	for (i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
 		{
-           scanf("%d",&matrix_1[i][j]);
+			scanf("%d", &matrix_1[i][j]);
 		}
 	}
-	printf("Enter the second matrix:- ");
-   	for (int i = 0; i < 3; i++)
+
+	printf("Enter the second matrix: ");
+	for (i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
 		{
-        scanf("%d",&matrix_2[i][j]);
+			scanf("%d", &matrix_2[i][j]);
 		}
 	}
+
 	printf("_____sum of two matrices______\n\n");
-	for (int i = 0; i < 3; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
 		{
-          printf("%d\t",matrix_1[i][j]+matrix_2[i][j]);
+			printf("%d\t", matrix_1[i][j] + matrix_2[i][j]);
 		}
 		printf("\n");
 	}
