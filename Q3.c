d39df1e74d629796bcedde6963736fe228d8e955
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:39:31 +0530

    Write a 'C' Program to accept 'n' numbers from user, store these numbers into an array.  Find out Largest and Smallest number from an array.

--- Q3.c
+++ Q3.c
@@ -1,29 +1,29 @@
 /*                                Program:- 3
-     Write a 'C' Program to accept 'n' numbers from user, store these numbers into an array. 
-     Find out Largest and Smallest number from an array.                           
-	                                                                                          */
-#include<stdio.h>
+	 Write a 'C' Program to accept 'n' numbers from user, store these numbers into an array.
+	 Find out Largest and Smallest number from an array.
+																							  */
+#include <stdio.h>
 void main()
 {
-	int n,i,max,min;
-	printf("what number do you want to search:- ");
-	scanf("%d",&n);
+	int n, i, max, j, min;
+	printf("What number do you want to enter: ");
+	scanf("%d", &n);
 	int num[n];
-	printf("Enter the %d integer number\n",n);
-    for ( i = 0; i <n; i++)
-     scanf("%d",&num[i]);
-	 max=num[0];
-	 min=num[0];
-	for (int j = 0; j <n; j++)
+	printf("Enter the %d integer number\n", n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &num[i]);
+	max = num[0];
+	min = num[0];
+	for (j = 0; j < n; j++)
 	{
-		if (max<num[j])
+		if (max < num[j])
 		{
-			max=num[j];
+			max = num[j];
 		}
-		if (min>num[j])
+		if (min > num[j])
 		{
-			min=num[j];
+			min = num[j];
 		}
 	}
-   printf("max= %d\nmin= %d",max,min);
+	printf("max= %d\nmin= %d", max, min);
 }
