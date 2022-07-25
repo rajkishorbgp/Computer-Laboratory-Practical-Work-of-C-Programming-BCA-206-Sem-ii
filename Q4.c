711253678849689a44687d910f9b12277a942654
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:41:04 +0530

    write a C program to read an array of 7 integer numbers and sort it in ascending order.

--- Q4.c
+++ Q4.c
@@ -1,27 +1,27 @@
 /*                               Program:-4
-   C program to read an array of 7 integer numbers and sort it in ascending order.
-                                                                                      */
-#include<stdio.h>
+   write a C program to read an array of 7 integer numbers and sort it in ascending order.
+																					  */
+#include <stdio.h>
 void main()
 {
-	int num[7],t;
-	printf("Enter the any 7 integer number\n");
+	int num[7], t, i, j;
+	printf("Enter the any 7 integer number: ");
 	for (int i = 0; i < 7; i++)
-		scanf("%d",&num[i]);
-	for (int i = 0; i<7; i++)
+		scanf("%d", &num[i]);
+	for (i = 0; i < 7; i++)
 	{
-	    for (int j = 0; j<7-i; j++)
+		for (j = 0; j < 7 - i; j++)
 		{
-		   if (num[j]>num[j+1])
-		    {
-				t=num[j];
-			   num[j]=num[j+1];
-			   num[j+1]=t;
-     		}
-		}	
+			if (num[j] > num[j + 1])
+			{
+				t = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = t;
+			}
+		}
 	}
-    for (int i = 0; i<7; i++)
+	for (i = 0; i < 7; i++)
 	{
-		printf("%d ",num[i]);
+		printf("%d ", num[i]);
 	}
 }
