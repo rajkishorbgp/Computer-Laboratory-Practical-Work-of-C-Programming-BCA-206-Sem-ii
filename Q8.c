7e282679df050d8917a443b72e3ac50067718dab
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:45:34 +0530

    C program to read matrix of size 3*3 and display transpose of matrix

--- Q8.c
+++ Q8.c
@@ -1,24 +1,27 @@
 /*                              Program:- 8
-     C program to read matrix of size 3*3 and display transpose of matrix
-                                                                          */
-#include<stdio.h>
+	 C program to read matrix of size 3*3 and display transpose of matrix
+																		  */
+#include <stdio.h>
 void main()
 {
-	int matrix[3][3],i,j;
+	int matrix[3][3], i, j;
+
 	printf("Enter the matrix :\n");
-	for ( i = 0; i <3; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for ( j = 0; j <3; j++)
+		for (j = 0; j < 3; j++)
 		{
-			scanf("%d",&matrix[i][j]);
+			scanf("%d", &matrix[i][j]);
 		}
 	}
+
 	printf("\n_____transpose of matrix_______\n");
-	for ( i = 0; i <3; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for ( j = 0; j <3; j++)
+		for (j = 0; j < 3; j++)
 		{
-         printf("%d\t",matrix[j][i]); // transpose of matrix 			
-		}printf("\n");
+			printf("%d\t", matrix[j][i]); // transpose of matrix
+		}
+		printf("\n");
 	}
 }
