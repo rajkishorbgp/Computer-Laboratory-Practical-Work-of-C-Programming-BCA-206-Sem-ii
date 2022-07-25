97913844ee055acb2ed09eb82606e65382687710
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:38:08 +0530

    C program to read an array of 8 integer and find sum of all even and odd numbers.

--- Q2.c
+++ Q2.c
@@ -1,18 +1,18 @@
 /*                                   Program:-2
-    C program to read an array of 8 integer and find sum of all even and odd numbers. 
-                                                                                            */
-#include<stdio.h>
+	C program to read an array of 8 integer and find sum of all even and odd numbers.
+																							*/
+#include <stdio.h>
 void main()
 {
-	int num[8],i,even=0,odd=0;
-	printf("Enter eight integer number:- ");
-	for ( i = 0; i <8; i++)
+	int num[8], i, even = 0, odd = 0;
+	printf("Enter eight integer number: ");
+	for (i = 0; i < 8; i++)
 	{
-		scanf("%d",&num[i]);
-		if (num[i]%2==0)
-			even+=num[i];
+		scanf("%d", &num[i]);
+		if (num[i] % 2 == 0)
+			even += num[i];
 		else
-		   odd+=num[i];
+			odd += num[i];
 	}
-	printf("sum of all even no= %d\nsum of all odd no= %d",even,odd);
+	printf("sum of all even no= %d\nsum of all odd no= %d", even, odd);
 }
