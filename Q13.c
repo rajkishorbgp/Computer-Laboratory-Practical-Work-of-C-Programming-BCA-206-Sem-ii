c27e185aba44c1fa1c6c230b41e350f1f66e41b7
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 02:21:40 +0530

    Write a c program to add two numbers using the concept of function pointer.

--- Q13.c
+++ Q13.c
@@ -1,18 +1,19 @@
 /*                                  Program: 13
-         Write a c program to add two numbers using the concept of function pointer.
-			                                                                                */
-#include<stdio.h>
-float add (float,float);
+		 Write a c program to add two numbers using the concept of function pointer.
+																							*/
+#include <stdio.h>
+float add(float, float);
 void main()
 {
-	float sum,a,b;
+	float sum, a, b;
 	printf("Enter the two number: ");
-	scanf("%f%f",&a,&b);
-    float (*ptr)(float,float);
-    ptr=add;
-	sum=(ptr)(a,b);
-	printf("Add two number: %f",sum);
+	scanf("%f%f", &a, &b);
+	float (*ptr)(float, float);
+	ptr = add;
+	sum = (ptr)(a, b);
+	printf("Sum of two no: %f", sum);
 }
-float add(float x,float y){
-	return (x+y);
+float add(float x, float y)
+{
+	return (x + y);
 }
