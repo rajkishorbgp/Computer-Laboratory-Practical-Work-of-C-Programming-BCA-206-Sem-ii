abc7fe010eacd5618fa46dade2c06c1a32434637
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 01:46:27 +0530

    Write a 'C' program to swap the values of two variables by using call by reference.

--- Q9.c
+++ Q9.c
@@ -1,22 +1,23 @@
 /*                             Program:- 9
-        Write a 'C' program to swap the values of two variables by using call by reference.
-                                                                           */
-#include<stdio.h>
-void swap( int *, int *);
+		Write a 'C' program to swap the values of two variables by using call by reference.
+																		   */
+#include <stdio.h>
+void swap(int *, int *);
 void main()
 {
-	int a,b;
+	int a, b;
 	printf("Enter the first number: ");
-	scanf("%d",&a);
+	scanf("%d", &a);
 	printf("Enter the second number: ");
-	scanf("%d",&b);
-	printf("_____Before swaping values_____\n\tA= %d\tB= %d",a,b);
-	swap(&a,&b);
-	printf("\n_____After swaping values_____\n\tA= %d\tB= %d",a,b);
+	scanf("%d", &b);
+	printf("_____Before swaping values_____\n\tA= %d\tB= %d", a, b);
+	swap(&a, &b);
+	printf("\n_____After swaping values_____\n\tA= %d\tB= %d", a, b);
 }
-void swap( int *x,int *y){
-       int z;
-	   z=*x;
-	   *x=*y;
-	   *y=z;
+void swap(int *x, int *y)
+{
+	int z;
+	z = *x;
+	*x = *y;
+	*y = z;
 }
