68231568e82e90a73e0c58088379011d50d5e483
Raj kishor <rajkishor6091@gmaio.com>
2022-07-26 02:23:30 +0530

    C program to read a string and check if it is palindrome.

--- Q15.c
+++ Q15.c
@@ -1,19 +1,19 @@
 /*                 program: 15
-      C program to read a string and check if it is palindrome.
-                                                                           */
-																		  
-#include<stdio.h>
-#include<string.h>
+	  C program to read a string and check if it is palindrome.
+																		   */
+
+#include <stdio.h>
+#include <string.h>
 void main()
 {
-	char string[100],recpy[100];
+	char string[100], recpy[100];
 	int com;
 	printf("Enter String: ");
 	gets(string);
-	strcpy(recpy,string);
+	strcpy(recpy, string);
 	strrev(string);
-    com =strcmp(string,recpy);
-	if (com==0)
+	com = strcmp(string, recpy);
+	if (com == 0)
 	{
 		printf("it is palindrome.");
 	}
