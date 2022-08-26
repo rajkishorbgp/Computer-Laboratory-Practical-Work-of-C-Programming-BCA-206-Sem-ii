09e437575e9724e550f62ab790e7a956d95ab87d
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-26 23:52:54 +0530

    Write a C program to reverse a string using recursive function.

--- Q16.c
+++ Q16.c
@@ -1,29 +1,31 @@
 /*                        Program: 16
          Write a C program to reverse a string using recursive function.
-		                                                                      */
-#include<stdio.h>
-void rev(char [],int ,int );
+                                                                          */
+#include <stdio.h>
+void rev(char[], int, int);
 void main()
 {
-	char ch,name[20];
+  char name[20];
   printf("Enter string: ");
   gets(name);
-  int count=0;
-  while (name[count]!='\0')
-    {
-       count++;
-    }
-  rev(name,0,count-1);
+  int count = 0;
+  while (name[count] != '\0')
+  {
+    count++;
+  }
+  rev(name, 0, count - 1);
   puts(name);
 }
 
-void rev(char arr[],int i, int l){
+void rev(char arr[], int i, int l)
+{
   char ch;
-  if(i>=l){
+  if (i >= l)
+  {
     return;
   }
-  ch=arr[i];
-  arr[i]=arr[l];
-  arr[l]=ch;
-  rev(arr,++i,--l);
+  ch = arr[i];
+  arr[i] = arr[l];
+  arr[l] = ch;
+  rev(arr, ++i, --l);
 }
