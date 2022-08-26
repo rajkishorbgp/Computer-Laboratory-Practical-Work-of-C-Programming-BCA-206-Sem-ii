8db58d0a2638632007def1d584dd41d1c8f280bc
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-26 23:55:30 +0530

    Write a C program to accept 5 names and store them into an array. Sort the elements in         alphabetical order.

--- Q18.c
+++ Q18.c
@@ -1,39 +1,39 @@
 /*                                    Program:18
-        Write a C program to accept 5 names and store them into an array. Sort the elements in 
-        alphabetical order. 
+        Write a C program to accept 5 names and store them into an array. Sort the elements in
+        alphabetical order.
 
                                                                                 */
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 void main()
 {
-        char name[5][30],t[30];
-        int i,j;
+        char name[5][30], t[30];
+        int i, j;
         printf("Enter the 5 names : ");
-        for (i =0; i<5; i++)
+        for (i = 0; i < 5; i++)
         {
                 gets(name[i]);
         }
         printf("\t_____Given names_____\n");
-        for ( i = 0; i <5; i++)
+        for (i = 0; i < 5; i++)
         {
                 puts(name[i]);
         }
-        for ( i = 0; i <5; i++)
+        for (i = 0; i < 5; i++)
         {
-              for ( j = 0; j <5-i-1; j++)
-              {
-                  if (strcmp(name[j],name[j+1])>0)
-                  {
-                        strcpy(t,name[j]);
-                        strcpy(name[j],name[j+1]);
-                        strcpy(name[j+1],t);
-                  }
-              }
+                for (j = 0; j < 5 - i - 1; j++)
+                {
+                        if (strcmp(name[j], name[j + 1]) > 0)
+                        {
+                                strcpy(t, name[j]);
+                                strcpy(name[j], name[j + 1]);
+                                strcpy(name[j + 1], t);
+                        }
+                }
         }
-        printf("\t______Sorted names______\n"); 
-        for ( i = 0; i <5; i++)
+        printf("\t______Sorted names______\n");
+        for (i = 0; i < 5; i++)
         {
-              puts(name[i]);
+                puts(name[i]);
         }
 }
